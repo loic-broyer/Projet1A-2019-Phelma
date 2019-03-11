0e1debe2c207ed7d93a66a2ac1234d5943b52a00
benanoob <soilihi@protonmail.com>
2019-03-11 10:11:08 +0100

    prog 06 en cours

---
+++ fonctionTests/seuillagecouleurtest.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+#CODAGE BGR DE SES MORTS PLUTOT QUE RGB
+
+
+
+# img = cv2.imread("test1.jpg")
+#
+# assert img is not None #check if image was loaded
+
+# print('original shape:', img.shape)
+# print('dtype:', img.dtype)
+#
+# width , height = 500, 500
+# WMult,HMult = 0.5, 0.5
+# resizedImg = cv2.resize(img, (width, height))
+
+
+# print("resized shape : ", resizedImg.shape)
+#
+# img_flip_along_x = cv2.flip(resizedImg, 0)
+#
+# cv2.imwrite("testsave1.jpeg",resizedImg,[cv2.IMWRITE_JPEG_QUALITY,80])
+#
+#
+# size = (img.shape[0:2])
+#
+#
+# cv2.imshow('original image',img)
+# cv2.waitKey(2000)
+
+
+sourceImg = cv2.imread("photo_test_1.jpg")
+sourceImg = cv2.resize(sourceImg,(500,500))
+assert sourceImg is not None
+
+HSVImg = cv2.cvtColor(sourceImg, cv2.COLOR_BGR2HSV)
+
+def nothing(x):
+    pass
+
+cv2.namedWindow('window')
+cv2.createTrackbar('Hmin','window',0,179,nothing)
+cv2.createTrackbar('Hmax','window',0,179,nothing)
+cv2.createTrackbar('Smin','window',0,255,nothing)
+cv2.createTrackbar('Smax','window',0,255,nothing)
+cv2.createTrackbar('Vmin','window',0,255,nothing)
+cv2.createTrackbar('Vmax','window',0,255,nothing)
+
+
+cv2.imshow("originale",HSVImg)
+
+
+
+while True:
+    Hmin = cv2.getTrackbarPos('Hmin','window')
+    Hmax = cv2.getTrackbarPos('Hmax','window')
+    Smin = cv2.getTrackbarPos('Smin','window')
+    Smax = cv2.getTrackbarPos('Smax','window')
+    Vmin = cv2.getTrackbarPos('Vmin','window')
+    Vmax = cv2.getTrackbarPos('Vmax','window')
+    Filtered = cv2.inRange(HSVImg, np.array([Hmin,Smin,Vmin]), np.array([Hmax,Smax,Vmax]))
+    cv2.imshow('window', Filtered)
+
+
+    key = cv2.waitKey(3)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
---
+++ prog_v06_decoupage_fonctions/fonctions_v0_6.py
@@ -0,0 +1,147 @@
+import cv2
+import numpy as np
+
+delta_x_min = 15#en nombre de pixels
+delta_x_max = 40
+delta_y_min = delta_x_min
+delta_y_max = delta_x_max
+perimetre_min = 3*delta_x_min
+perimetre_max = 3*delta_x_max
+aire_min = delta_x_min**2
+
+
+def centroid(contour):
+    """Returns the coordinate X and Y of the centroid of the input contour in the form of a tuple"""
+    M = cv2.moments(contour)
+    #print(contour)
+    #print("AAAAAAAAAAAa")
+    if (M['m00'] !=0 ):
+        Cx = int(M['m10']/M['m00'])
+        Cy = int(M['m01']/M['m00'])
+        return(Cx,Cy)
+
+
+def threshold(img,Hmin=0,Hmax=179,Smin=0,Smax=255,Vmin=0,Vmax=255):
+    HSVImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return cv2.inRange(HSVImg, np.array([Hmin,Smin,Vmin]), np.array([Hmax,Smax,Vmax]))
+
+
+
+def tri_contour_taille(liste_contour):
+    #Tri des contours suivant leur périmètre et leur taille en x et en y:
+    liste_contour_tries=[]
+    for i in range(len(liste_contour)):
+        if len(liste_contour[i])>=perimetre_min and len(liste_contour[i])<=perimetre_max:
+            print("contour n° "+str(i))
+            liste_contour_tries.append(liste_contour[i])
+    return liste_contour_tries
+
+def tri_contour_aire(liste_contour):
+    #Tri des contours
+    #supprime les contours qui ont une aire trop petite
+    liste_contour_tries=[]
+    for i in range(len(liste_contour)):
+        if cv2.contourArea(liste_contour[i]) >= aire_min:
+            liste_contour_tries.append(liste_contour[i])
+    return liste_contour_tries
+
+def affiche_contour_couleurs_differentes(liste_contour, nb_pixel_x, nb_pixel_y):
+    #prend en paramètre les dimensions de l'image dont on a pris les contours
+    #affiche tous les contours avec une couleur différente par contour
+    img_tous_les_contours = np.full((nb_pixel_x, nb_pixel_y, 3), 0, np.uint8)
+    for i in range(len(liste_contour)):
+        a=(15*i)%256
+        b=(22*i)%256
+        c=(9*i)%256
+        if a+b+c<80:#pour ne pas avoir de contour trop sombre
+            a+=30
+            b+=30
+            c+=30
+        couleur=[a,b,c]
+        for j in range(len(liste_contour[i])):
+            x=liste_contour[i][j][0][1]
+            y=liste_contour[i][j][0][0]
+            img_tous_les_contours[x][y]=couleur
+    cv2.imshow('tous les contours', img_tous_les_contours)
+    cv2.imwrite("contours.png", img_tous_les_contours)
+
+
+def detecte_palets(img, couleur="vert", affichage=0):
+    #retourne une liste contenant les centres de tous les palets de la couleur demandée
+    #passer 0 en paramètre d'affichage pour ne pas afficher les images intermédiaires
+    assert couleur in ["rouge", "vert", "bleu"]
+
+    ##########################################################
+    #Prise des contours de l'image
+    if couleur == "vert":
+        img = seuillageCouleur(img,29,74,20)
+    elif couleur == "bleu":
+        img = seuillageCouleur(img,81,102,50)
+    else:
+        img = seuillageCouleur(img,81,102,50)
+
+    img_canny = cv2.Canny(img, 50, 100, 3)
+    if affichage != 0:
+        cv2.imshow('image seuillée pour le '+couleur, img)
+        cv2.imshow('image passée au filtre de Canny pour le '+couleur, img_canny)
+        cv2.waitKey(0)
+
+    #########################################################
+    #Regroupement des pixels détectés par contour
+    liste_contour, _ = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    if affichage != 0:
+        affiche_contour_couleurs_differentes(liste_contour, img.shape[0], img.shape[1])
+        cv2.waitKey(0)
+
+    #########################################################
+    #Tri des contours
+    liste_contour_tries=tri_contour_aire(tri_contour_taille(liste_contour))
+
+    ###########################################################
+    #affichage des contours triés
+    img_contours = np.full((img.shape[0], img.shape[1]), 0, np.uint8)
+    for i in range(len(liste_contour_tries)):
+        for j in range(len(liste_contour_tries[i])):
+            x=liste_contour_tries[i][j][0][1]
+            y=liste_contour_tries[i][j][0][0]
+            #img[x][y]=[255,255,255]-img[x][y]
+            img_contours[x][y]=255
+#    cv2.imshow('image initiale + contours triés', img)
+    if affichage != 0:
+        cv2.imshow('image initiale + contours triés', img_contours)
+        cv2.waitKey(0)
+
+    ##########################################################
+    #Calcule le centre des contours
+    liste_centre_palets=np.full((len(liste_contour_tries), 2), 0, np.uint32)
+    for i in range(len(liste_contour_tries)):
+        centre_x=0
+        centre_y=0
+        for j in range(len(liste_contour_tries[i])):
+            centre_x += liste_contour_tries[i][j][0][1]
+            centre_y += liste_contour_tries[i][j][0][0]
+        liste_centre_palets[i][1] = centre_x//len(liste_contour_tries[i])
+        liste_centre_palets[i][0] = centre_y//len(liste_contour_tries[i])
+        if affichage == 1:
+            print(centre_x,len(liste_contour_tries[i]),liste_centre_palets[i][1])
+            print(centre_y,len(liste_contour_tries[i]),liste_centre_palets[i][0])
+
+    #rajoute le centre sur l'image des contours
+    for i in range(len(liste_centre_palets)):
+        img_contours[liste_centre_palets[i][1]][liste_centre_palets[i][0]]=255
+
+    if affichage != 0:
+        cv2.imshow('contour triés', img_contours)
+        cv2.waitKey(0)
+
+    ###########################################################
+    """
+    cv2.imwrite(nom_fichier+"_canny.png", img_canny)
+    cv2.imwrite(nom_fichier+"_gris.png", img)
+    cv2.imwrite(nom_fichier+"_contours_seuls.png", img_contours)
+    #cv2.imwrite(nom_fichier+"_image_et_contours.png", img_couleur)
+    """
+
+    print("initialement :",len(liste_contour),"contours")
+    print(len(liste_contour_tries),"contours sélectionnés")
+    return liste_centre_palets
