c4d541b4d8283c60d3ddcee92ed9b0351664af30
hectorphelma <47922985+hectorphelma@users.noreply.github.com>
2019-04-08 08:23:19 +0200

    Update est-ce_que_le_point_y_est.py

--- prog_v07/est-ce_que_le_point_y_est.py
+++ prog_v07/est-ce_que_le_point_y_est.py
@@ -14,7 +14,23 @@ def point_commun(liste1,liste2,liste3,tolérance):
     for L1 in range(len(liste1)):
         for L2 in range(len(liste2)):
             for L3 in range (len(liste3)):
-                if (distance(liste1[L1],liste2[L3])<tolerance):
+                if (distance(liste1[L1],liste2[L2])<tolerance):
                     if (distance(liste2[L2],liste3[L3])<tolerance):
                         communs.append(moyenne(liste1[L1],liste2[L2],liste3[L3]))
     return(communs)
+##si on souhaite que présent dans les trois images
+
+##sinon on peut se contenter que le palets soit dans deux des trois images
+def point_commun2(liste1,liste2,liste3,tolérance):
+    communs=[]
+    nbconcordance=0
+    for L1 in range(len(liste1)):
+        for L2 in range(len(liste2)):
+            for L3 in range (len(liste3)):
+                if (distance(liste1[L1],liste2[L2])<tolerance):
+                    nbconcordance+=1
+                if (distance(liste2[L2],liste3[L3])<tolerance):
+                    nbconcordance+=1
+                if (nbconcordance>=2):
+                    communs.append(moyenne(liste1[L1],liste2[L2],liste3[L3]))
+    return(nbconcordance)
