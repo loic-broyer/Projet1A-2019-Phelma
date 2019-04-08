8da6221863a51e2838fa3eddd79934431017e8f7
hectorphelma <47922985+hectorphelma@users.noreply.github.com>
2019-04-08 11:19:38 +0200

    Update correction_hauteur_robot.py

--- prog_v07/correction_hauteur_robot.py
+++ prog_v07/correction_hauteur_robot.py
@@ -3,11 +3,36 @@ import numpy as np
 def distance(long1,larg1,long2,larg2):
     return(np.sqrt((larg1-larg2)**2+(long1-long2)**2))
 
-def init_correction_robot(longueur,largeur):
-    abaque=[] ## il s'agit d'une liste dans laquelle on répertorie des points
+## on peut commencer par définir une fonction de création de la liste abaque
+     ## il s'agit d'une liste dans laquelle on répertorie des points
     ## de mesure sous la forme: [abscisse,ordonnée,décalage en abscisse,décalage en ordonnée]
     ##l'algorithme doit chercher lui-même le point de mesure le plus proche pour savoir quelle correction appliquer
 
+def remplissage():
+    abaque=[]
+    a='oui'
+    while(a=='oui'):
+        print("voulez-vous ajouter une mesure (répondre 'oui' ou 'non'")
+        a=input()
+        if(a=='oui'):
+            mesure=[]
+            print("donnez l'abscisse de la mesure")
+            b=int(input())
+            mesure.append(b)
+            print("donnez l'ordonnée de la mesure")
+            b=int(input())
+            mesure.append(b)
+            print("donnez le décalage en abscisse en ce point")
+            b=int(input())
+            mesure.append(b)
+            print("donnez le décalage en ordonnée en ce point")
+            b=int(input())
+            mesure.append(b)
+            abaque.append(mesure)
+    return(abaque)
+    
+
+def init_correction_robot(longueur,largeur,abaque):
     correction=np.zeros((longueur,largeur,2),[])
     ## cette matrice aux dimension des images traitées va être complétée avec les coordonnées des points de mesure les plus proches:
     ##Ainsi, on aura plus qu'à consulter le point de même coordonnées dans cette matrice pour savoir la correction à apporter à la position du robot sur l'imageacquise
@@ -34,16 +59,18 @@ def init_correction_robot(longueur,largeur):
             ordonnéenegmin=0
             ordonnéeposmin=0
             
-            while(contour[longueur-abscissenegmin][largeur][0]==0):
+            while(correction[longueur-abscissenegmin][largeur][0]==0):
                 abscissenegmin+=1
-            while(contour[longueur+abscisseposmin][largeur][0]==0):
+            while(correction[longueur+abscisseposmin][largeur][0]==0):
                 abscisseposmin+=1            
-            while(contour[longueur][largeur-ordonnéenegmin][0]==0):
+            while(correction[longueur][largeur-ordonnéenegmin][0]==0):
                 ordonnéenegmin+=1
-            while(contour[longueur][largeur+ordonnéeposmin][0]==0):
+            while(correction[longueur][largeur+ordonnéeposmin][0]==0):
                 ordonnéenegmin+=1
 
-            correction[long][larg][0]=(correction[long-abscissenegmin][larg][0]+correction[long+abscisseposmin][larg][0])/(abscisseposmin-abscissenegmin)
-            correction[long][larg][1]=(correction[long][larg-ordonnéenegmin][1]+correction[long][larg+ordonnéeposmin][1])/(ordonnéeposmin-ordonnéenegmin)
+            correction[long][larg][0]=(correction[long-abscissenegmin][larg][0]+correction[long+abscisseposmin][larg][0])/(abscisseposmin-abscissenegmin)//1
+            correction[long][larg][1]=(correction[long][larg-ordonnéenegmin][1]+correction[long][larg+ordonnéeposmin][1])/(ordonnéeposmin-ordonnéenegmin)//1
             
     return(correction)
+## on a alors crée la matrice de correction pour la hauteur du robot
+## on peut alors décider de l'inscrire dans un document texte si on ne veut pas la réinitialiser à chaque match
