17125e263793e5c9074f19c09d57e04d900c0910
Loic <loic.broyer@grenoble-inp.org>
2019-05-22 17:08:05 +0200

    code reseau

--- prog_v08/server.py
+++ prog_v08/server.py
@@ -5,54 +5,66 @@ import pickle
 import sys
 
 usedPort = 1111
-requestSize = 10000 #Taille de l'objet requete
+requestSize = 4	#Taille de l'objet requete
 
 #Classe d'objet requete
 class requestObject():
 
-    def __init__(self, robot, reqType, argument):
-        self.reqType = reqType
-        self.robot = robot
-        self.argument = argument
+	def __init__(self, robot, reqType):
+		self.reqType = reqType
+		self.robot = robot
 
 #Initialisation (ouverture de la socket)
 def initServer(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("", port))
-    print("Server is ready")
-    return sock
+    
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.bind(("", port))
+	print("Server is ready")
+	return sock
 
-#Routine (écoute des requetes reseau)
-def routineServer(sock):
+#i le nombre à envoyer et size la taille du buffer (1 = 0001 pour size = 4)
+def wifiFormatInt(i, size):
+	string = str(i)
+	if(len(string) > size):
+		print("Erreur : int trop grand")
+	elif(len(string) < size):
+		diff = size - len(string)
+		for k in range(0, diff):
+			string = "0" + string
+	return string
+
+#[[[x,y],[x,y]], [liste bleue], [liste verte]]
+def envoiDesCoordonnees(sock, cList):
+	len1 = len(cList)
+	sock.send(wifiFormatInt(len1, 5).encode())
+	for i in range(0,len1):	#Les listes de couleur
+		len2 = len(cList[i])
+		sock.send(wifiFormatInt(len2, 5).encode())
+		for j in range(0,len2):
+			sock.send(wifiFormatInt(cList[i][j][0], 5).encode())
+			sock.send(wifiFormatInt(cList[i][j][1], 5).encode())
+
+#Routine (écoute des requetes reseau), cList la liste des coordonnées
+def routineServer(sock, cList):
+    
 	#Ecoute reseau
 	sock.listen(10)
 	(clientsocket, (ip, port)) = sock.accept()
 	print("Connexion from %s %s" % (ip, port))
 	#Reception de l'objet requete
+	reqObj = requestObject(0,0)
 	data = clientsocket.recv(requestSize)
-	if(sys.getsizeof(data) != 0):
-		while(not sys.getsizeof(data) == requestSize):
-			data+=clientsocket.recv(requestSize)
-		reqObj = pickle.loads(data)
-		#Reponse à la requete
-		if(reqObj.reqType == 0):	#Pull setup status
-			data_str = pickle.dumps(gameVariables[0])
-			clientsocket.send(data_str)
-		elif(reqObj.reqType == 1):	#Push setup status
-			gameVariables[0][reqObj.robot] = 1
-		elif(reqObj.reqType == 2):	#Pull score
-			data_str = pickle.dumps(gameVariables[1])
-			clientsocket.send(data_str)
-		elif(reqObj.reqType == 3):	#Push score (TODO : voir si on a besoin de controle)
-			gameVariables[1] += reqObj.argument
-		elif(reqObj.reqType == 4):	#Pull coords
-			data_str = pickle.dumps(gameVariables[2])
-			clientsocket.send(data_str)
-		elif(reqObj.reqType == 5):	#Push coords (TODO : voir si on a besoin de controle)
-			gameVariables[2][reqObj.robot] = reqObj.argument
-		else:
-			print("Erreur : Code requete inconnu.")
+	reqObj.reqType = int(data.decode())
+	data = clientsocket.recv(requestSize)
+	reqObj.robot = int(data.decode())
+	#Reponse à la requete
+	if(reqObj.reqType == 0):	#Pull score
+		print("Fonction pas encore faite ou ne sera jamais faite mdr")
+	elif(reqObj.reqType == 1):	#Pull coords
+		envoiDesCoordonnees(sock, cList)
+	else:
+		print("Erreur : Code requete inconnu.")
 
 #Routine d'attente (requiert un spam constant du robot pour marcher)
 def netWaitForStart(sock):
@@ -60,20 +72,20 @@ def netWaitForStart(sock):
 	(clientsocket, (ip, port)) = sock.accept()
 	print("Connexion from %s %s" % (ip, port))
 	#Reception de l'objet requete
+	reqObj = requestObject(0,0)
 	data = clientsocket.recv(requestSize)
-	if(sys.getsizeof(data) != 0):
-		while(not sys.getsizeof(data) == requestSize):
-			data+=clientsocket.recv(requestSize)
-		reqObj = pickle.loads(data)
-		if(reqObj.reqType == 6):
-			return 1
-		else:
-			return 0
+	reqObj.reqType = int(data.decode())
+	data = clientsocket.recv(requestSize)
+	reqObj.robot = int(data.decode())
+	if(reqObj.reqType == 2):
+		sock.send(wifiFormatInt(1, 5).encode())
+		return 1
 	else:
+		sock.send(wifiFormatInt(0, 5).encode())
 		return 0
 
-#main
+#main 
 #sock = initServer(usedPort)
 #while True:
-#   routineServer(sock)
+#	routineServer(sock)
 
